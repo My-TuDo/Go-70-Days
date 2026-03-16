87b2578c7a85082b50bb82249c9d91b4c00e78da
My-TuDo <2647601953@qq.com>
2026-03-16 16:51:07 +0800

    feat: my first go code

---
+++ day01/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var GO = "会赢的！！"
+
+func main()  {
+
+	name := "谢成功"
+
+	fmt.Println(name)
+	fmt.Println(GO)
+	fmt.Println("Hello,World!")
+	fmt.Println("Hello,Go!")
+	fmt.Println("Hello,Ubuntu!")
+}
