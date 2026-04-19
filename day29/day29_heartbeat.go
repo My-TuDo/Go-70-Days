bf41937ef9eb02d3b07aa8113c9e49b7c514d958
My-TuDo <2647601953@qq.com>
2026-04-19 21:07:49 +0800

    feat: build periodic health check logic using time.Ticker and select multiplexing

--- day29/day29_heartbeat.go
+++ day29/day29_heartbeat.go
@@ -7,17 +7,24 @@ import (
 
 func main() {
 	// 1. 定义一每 2 秒响一次的闹钟
+	// Ticker 会持有一个 Channel ，每隔固定时间往里面塞一个时间戳
 	ticker := time.NewTicker(2 * time.Second)
 	defer ticker.Stop()
 
 	// 2. 定义一个 10 秒后自毁的定义器（模拟系统维护）
-	done := time.After(11*time.Second)
+	// After 也会返回一个 Channel， 10 秒后只发送一次信号
+	done := time.After(11 * time.Second)
 
 	fmt.Println("系统心跳监测已启动...")
+	// 3. 多路复用循环
 	for {
 		select {
+		// 情况一：每当 ticker 的 Channel 收到一个时间戳，就上报一次系统状态
 		case t := <-ticker.C:
+			// 如果是生产环境，这里可以改成上报实际的系统状态，比如 CPU 和内存使用率
+			// 然后发往 Prometheus 或是存入 MySQL
 			fmt.Printf("[%v] 上报数据：CPU 2。5%%， 内存使用率 45%%， 状态：OK\n", t.Format("15:04:05"))
+		// 情况二：当 done 的 Channel 收到信号，说明系统维护时间到了，停止心跳监测
 		case <-done:
 			fmt.Println("系统维护中，心跳监测已停止。")
 			return
