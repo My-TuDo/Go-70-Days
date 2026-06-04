628f3e38536e0df0f5309803953e66827abf9b99
My-TuDo <2647601953@qq.com>
2026-06-04 21:50:47 +0800

    fix(kvault): server/main.go 补全实现

--- kvault/cmd/server/main.go
+++ kvault/cmd/server/main.go
@@ -1,1 +1,24 @@
-package server
+package main
+
+import (
+	"kvault/api/kvaultpb"
+	"kvault/internal/server"
+	"kvault/internal/store"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	s := store.New()                              // 创建存储实例
+	KvSrv := server.New(s)                        // 创建服务器实例，注入存储
+	grpcSrv := grpc.NewServer()                   // 创建 gRPC 服务器
+	kvaultpb.RegisterKVaultServer(grpcSrv, KvSrv) // 注册 KVault 服务 到 gRPC 服务器
+
+	lis, err := net.Listen("tcp", ":50051") // 监听 TCP 端口
+	if err != nil {
+		log.Fatalf("监听端口失败: %v", err)
+	}
+	grpcSrv.Serve(lis) // 启动 gRPC 服务器
+}
